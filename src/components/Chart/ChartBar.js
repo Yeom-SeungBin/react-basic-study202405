90cb2889d921052288c8105ce466a7d4ca94c9a2
sseung <duaqkr2@naver.com>
2024-05-03 11:42:45 +0900

    12. chart 구현 완료

--- src/components/Chart/ChartBar.js
+++ src/components/Chart/ChartBar.js
@@ -1,13 +1,24 @@
 import React from 'react';
 import './ChartBar.css';
 
-const ChartBar = () => {
+const ChartBar = ({ label, currentValue, totalValue }) => {
+  let barFillHeight = '0%';
+
+  if (totalValue > 0) {
+    // 한 번도 지출하지 않았을 경우 totalValue가 0이기 때문에 에러가 날 수 있음.
+    const percentage = (currentValue / totalValue) * 100;
+    barFillHeight = percentage + '%';
+  }
+
   return (
     <div className="chart-bar">
       <div className="chart-bar__inner">
-        <div className="chart-bar__fill"></div>
+        <div
+          className="chart-bar__fill"
+          style={{ height: barFillHeight }}
+        ></div>
       </div>
-      <div className="chart-bar__label"></div>
+      <div className="chart-bar__label">{label}</div>
     </div>
   );
 };
