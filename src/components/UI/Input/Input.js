2b5b3ad5b9db2e50068dbcc19540d354143fe8d0
sseung <duaqkr2@naver.com>
2024-05-08 11:08:46 +0900

    Food 템플릿 작성 중 (Header 부분)

--- src/components/UI/Input/Input.js
+++ src/components/UI/Input/Input.js
@@ -3,7 +3,9 @@ import styles from './Input.module.css';
 
 const Input = ({ id, label, type, value, isValid, ...rest }) => {
   return (
-    <div className={`${styles.control} ${!isValid ? styles.invalid : ''}`}>
+    <div
+      className={`${styles.control} ${!isValid ? styles.invalid : ''}`}
+    >
       <label htmlFor={id}>{label}</label>
       <input type={type} id={id} value={value} {...rest} />
     </div>
